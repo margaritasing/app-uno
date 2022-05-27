079438daa769606a8ae47587b2dc9237faf457f3
margaritasing <libiamfreitesm@gmail.com>
2022-05-27 19:05:29 -0300

    agrego constructor

--- src/main/java/com/margaritasing/GestorFactura.java
+++ src/main/java/com/margaritasing/GestorFactura.java
@@ -3,10 +3,12 @@ package com.margaritasing;
 public class GestorFactura {
 
     Calculadora calculadora;
+    String nombre;
 
-    public GestorFactura( Calculadora calculadora) {
+    public GestorFactura( Calculadora calculadora, String nombre) {
         System.out.println("Ejecutando constructor gestor");
         this.calculadora = calculadora;
+        this.nombre = nombre;
 
     }
 
--- src/main/java/com/margaritasing/Main.java
+++ src/main/java/com/margaritasing/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args){
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
         // Crear un objeto de forma normal
-        Calculadora service = new Calculadora();
+        //Calculadora service = new Calculadora();
 
         // Crear un objeto a partir de spring
         Calculadora calculadora = (Calculadora) context.getBean("calculadora");
@@ -22,6 +22,12 @@ public class Main {
         text = calculadora1.HolaMundo();
 
         System.out.println(text);
+
+        //Ejemplo 2
+
+       GestorFactura gestor = (GestorFactura) context.getBean("gestorFacturas");
+       System.out.println(gestor.calculadora.HolaMundo());
+
 
 
 
